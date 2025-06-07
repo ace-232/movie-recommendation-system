909feb36408ab1e49ef6e92dcee1a11954b08e81
Puja <pujad5359@gmail.com>
2025-06-08 05:16:53 +0530

    Modified app.py

--- backend/app.py
+++ backend/app.py
@@ -1139,5 +1139,6 @@ def handle_recommendations():
         }), 500
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
